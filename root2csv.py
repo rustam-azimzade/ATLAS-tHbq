630f58f739b0451ae95d2acad1a7f8cdf94d2b99
Rustam <azimzadeh.rustam@gmail.com>
2024-05-29 01:47:14 +0400

    have converted into pandas dataframe before exporting

--- root2csv.py
+++ root2csv.py
@@ -13,25 +13,35 @@ def main():
 
 
 def root2csv(where_root: str, where_csv: str) -> None:
-
+    extracted_data = dict()
     strip_encoding = lambda name: name.split(';')[0]
 
     try:
         # Extracting data from .root file
         with uproot.open(where_root) as root_file:
+
             tree_names = root_file.keys()
             for tree_name in tree_names:
                 tree_name = strip_encoding(tree_name)
                 logging.info(f"Processing tree: {tree_name}")
                 tree = root_file[tree_name]
+                tree_data = dict()
+
                 branch_names = tree.keys()
-                tree_data = tree.arrays(branch_names, library="ak")
-                tree_data_frame = pd.DataFrame({branch: tree_data[branch].tolist() for branch in branch_names})
-                tree_data_frame.to_csv(where_csv, index=False)
-                logging.info(f"Data have been written to {where_csv}")
+                for branch_name in branch_names:
+                    branch_name = strip_encoding(branch_name)
+                    logging.info(f"\tProcessing branch: {branch_name}")
+                    branch_data = tree[branch_name].arrays(library="ak").to_list() #none, library="np"
+                    tree_data[branch_name] = branch_data
+
+                extracted_data[tree_name] = tree_data
+
+        # Writing data to a JSON file
+        extracted_data_frame = pd.DataFrame(extracted_data)
+        extracted_data_frame.to_csv(where_csv, index=False)
+        logging.info(f"Data have been written to {where_csv}")
     except Exception as exception_info:
-        logging.error(f"An error occurred: {exception_info}")
-        return
+        logging.error(exception_info)
 
 
 if __name__ == '__main__':
--- root2json.py
+++ root2json.py
@@ -14,34 +14,37 @@ def main():
     root2json(ROOT_PATH, JSON_PATH)
 
 
-def strip_encoding(name: str) -> str:
-    return name.split(';')[0]
-
-
 def root2json(where_root: str, where_json: str) -> None:
     extracted_data = dict()
+    strip_encoding = lambda name: name.split(';')[0]
 
     try:
         # Extracting data from .root file
         with uproot.open(where_root) as root_file:
-            for tree_name in root_file.keys():
+
+            tree_names = root_file.keys()
+            for tree_name in tree_names:
                 tree_name = strip_encoding(tree_name)
                 logging.info(f"Processing tree: {tree_name}")
                 tree = root_file[tree_name]
                 tree_data = dict()
-                for branch_name in tree.keys():
+
+                branch_names = tree.keys()
+                for branch_name in branch_names:
                     branch_name = strip_encoding(branch_name)
                     logging.info(f"\tProcessing branch: {branch_name}")
-                    branch_data = tree[branch_name].arrays()
-                    #branch_data = tree[branch_name].arrays().tolist()
-                    #branch_data = tree[branch_name].arrays(library="np")
-                    # branch_data = tree[branch_name].arrays(library="pd")
-                    # branch_data = tree[branch_name].arrays(library="ak")
+                    branch_data = tree[branch_name].arrays(library="ak").tolist() # library=None library="np" library="pd"
                     tree_data[branch_name] = branch_data
+
                 extracted_data[tree_name] = tree_data
+
+        # Writing data to a JSON file
+        data_frame = pd.DataFrame(extracted_data)
+        data_frame.to_json(where_json, index=False)
+        logging.info(f"Data have been written to {where_json}")
+
     except Exception as exception_info:
-        logging.error(f"An error occurred: {exception_info}")
-        return
+        logging.error(exception_info)
 
     # Writing data to a JSON file
     data_frame = pd.DataFrame(extracted_data)
