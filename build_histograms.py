5bf82527dbf31310e834a6813b4b6959715cfa51
Rustam <azimzadeh.rustam@gmail.com>
2024-06-03 00:18:02 +0400

    made some corrections

--- build_histograms.py
+++ build_histograms.py
@@ -1,8 +1,8 @@
+import os
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import scienceplots
-import os
 from config import Config
 
 
@@ -24,20 +24,21 @@ def histogram(data, name):
     x_min = branch_data.min()
     x_max = branch_data.max()
     mean_value = branch_data.mean()
-    bins_count = np.histogram_bin_edges(branch_data, bins='auto') # auto, fd, doane, scott, stone, rice, sturges, sqrt
     standard_deviation = branch_data.std()
+    bins_count = np.histogram_bin_edges(branch_data, bins='sqrt') # auto, fd, scott, sqrt
 
     plt.style.use(['science', 'notebook', 'grid'])
     plt.figure(figsize=(10, 6))
-    plt.hist(branch_data, density=True, histtype='step', bins=bins_count, range=(x_min, x_max), label=name)
-    plt.plot([], [], ' ', label=f'Mean: {mean_value:.3f}')
-    plt.plot([], [], ' ', label=f'Std Dev: {standard_deviation:.3f}')
     plt.title(f'Histogram of {name}')
     plt.tick_params(axis='both', labelsize=10)
     #plt.xlabel('')
-    #plt.xticks(np.linspace(x_range[0], x_range[1], num=bins_count+1))
+    #plt.xticks(np.linspace(x_min, x_max, num=50))
     plt.xlim(x_min, x_max)
-    plt.ylim(0)
+    #plt.ylabel('')
+    #plt.ylim(0)
+    plt.hist(branch_data, density=True, histtype='step', bins=bins_count, range=(x_min, x_max), label=name)
+    plt.plot([], [], ' ', label=f'Mean: {mean_value:.3f}')
+    plt.plot([], [], ' ', label=f'Std Dev: {standard_deviation:.3f}')
     plt.legend(loc='upper right', fontsize=10, fancybox=False, edgecolor='black')
     plt.savefig(f'{Config.FOLDER_NAME}/{name}.png', dpi=200)
 
