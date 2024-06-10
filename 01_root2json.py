fe9eea300d23bc3db55e52c76a35335f84f32fea
Rustam <azimzadeh.rustam@gmail.com>
2024-06-10 15:47:58 +0400

    improved root2json

--- 01_root2json.py
+++ 01_root2json.py
@@ -9,25 +9,22 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 def main():
     ROOT_PATH = 'data/root/single_t_weighted.root'
-    JSON_PATH = ROOT_PATH.replace('root', 'json')
-    root2json(ROOT_PATH, JSON_PATH)
+    root2json(ROOT_PATH)
 
 
-def root2json(where_root: str, where_json: str) -> None:
+def root2json(where_root: str) -> None:
     extracted_data = dict()
-    strip_encoding = lambda name: name.split(';')[0]
 
     try:
         # Extracting data from .root file
         with uproot.open(where_root) as root_file:
+
             for tree_name in root_file.keys():
-                tree_name = strip_encoding(tree_name)
                 logging.info(f"Processing tree: {tree_name}")
                 tree = root_file[tree_name]
-                tree_data = dict()
+
                 for branch_name in tree.keys():
                     if branch_name in Config.NECESSARY_BRANCHES:
-                        branch_name = strip_encoding(branch_name)
                         logging.info(f"\tProcessing branch: {branch_name}")
                         branch_data = tree[branch_name].array(library="ak").to_list()
 
@@ -39,13 +36,15 @@ def root2json(where_root: str, where_json: str) -> None:
                                 if not branch_data[i]:
                                     branch_data[i] = None
 
-                        tree_data[branch_name] = branch_data
-                extracted_data[tree_name] = tree_data
+                        extracted_data[branch_name] = branch_data
 
-        # Writing data to a JSON file
-        with open(where_json, 'w', encoding='utf-8') as json_file:
-            json.dump(extracted_data, json_file, ensure_ascii=False)
-        logging.info(f"Data have been written to {where_json}")
+                # Writing data to a JSON file
+                json_path_parts = where_root.replace('root', 'json').rsplit('.', 1)
+                where_json = f'{json_path_parts[0]}_({tree_name}).{json_path_parts[1]}'
+
+                with open(where_json, 'w', encoding='utf-8') as json_file:
+                    json.dump(extracted_data, json_file, ensure_ascii=False)
+                logging.info(f"Data have been written to {where_json}\n")
 
     except Exception as exception_info:
         logging.error(exception_info)
