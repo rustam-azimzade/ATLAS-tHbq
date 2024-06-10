d3c3b1cf2a62c90dc59a066e5701157244570f82
Rustam <azimzadeh.rustam@gmail.com>
2024-06-10 15:46:35 +0400

    improved normalyze

--- 02_normalyze.py
+++ 02_normalyze.py
@@ -1,47 +1,35 @@
-# Каждое дерево в отдельный файл
-# преобразовать типы данных
-
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+import matplotlib
+import scienceplots
 from config import *
 import pandas as pd
-import scienceplots
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
 
 
 def main():
-    data = parse_json('data/json/single_t_weighted.json', Config.NECESSARY_TREE)
-    data = data[['chi2_min_higgs_m', 'nbjets']]
+    data = pd.read_json('data/json/MiniNtuple_tHbq_SM_300K_(aTTreethbqSM;1).json')
+    branch_name = 'higgs_m'
+    data = data[[branch_name]]
 
-    normalized_data = normalize(data)
-    standardized_data = standardize(data)
-
-    branch_name = 'chi2_min_higgs_m'
-    histogram(data[branch_name], normalized_data[branch_name], standardized_data[branch_name], branch_name)
-
-
-def parse_json(path, tree):
-    with open(path, 'r') as file:
-        data = pd.read_json(file)
-
-    data = data[tree].apply(pd.Series)
-    return data.transpose()
+    normalized_signal, max_value, min_value = normalize(data)
+    transformed_background = normalize(data, max_value, min_value)
+    histogram(data[branch_name], normalized_signal[branch_name], branch_name)
 
 
-def normalize(data):
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    normalized_data = scaler.fit_transform(data)
-    return pd.DataFrame(normalized_data, columns=data.columns, index=data.index)
+def normalize(data, max_value=None, min_value=None):
+    if not isinstance(data, pd.DataFrame):
+        data = data.to_frame()
+
+    if max_value is None and min_value is None:
+        max_value = data.max()
+        min_value = data.min()
+
+    data = (data - min_value) / (max_value - min_value)
+    return data, max_value, min_value
 
 
-def standardize(data):
-    scaler = StandardScaler()
-    standardize_data = scaler.fit_transform(data)
-    return pd.DataFrame(standardize_data, columns=data.columns, index=data.index)
-
-
-def histogram(data_before, data_normalized, data_standardized, name):
+def histogram(data_before, data_normalized, name):
     mean_value_before = data_before.mean()
     standard_deviation_before = data_before.std()
     bins_count_before = np.histogram_bin_edges(data_before, bins='scott') # auto, fd, scott
@@ -50,14 +38,17 @@ def histogram(data_before, data_normalized, data_standardized, name):
     standard_deviation_normalized = data_normalized.std()
     bins_count_normalized = np.histogram_bin_edges(data_normalized, bins='scott')  # auto, fd, scott
 
-    mean_value_standardized = data_standardized.mean()
-    standard_deviation_standardized = data_standardized.std()
-    bins_count_standardized = np.histogram_bin_edges(data_standardized, bins='scott')  # auto, fd, scott
-
     plt.style.use(['science', 'notebook', 'grid'])
-    figure, axes = plt.subplots(1, 3, figsize=(15, 3.5))
+    matplotlib.rcParams["axes.formatter.limits"] = (-1, 1)
+    formatter = ScalarFormatter(useMathText=True)
+    formatter.set_scientific(True)
+    figure, axes = plt.subplots(1, 2, figsize=(10, 3.5))
 
     axis1 = axes[0]
+    #axis1.xaxis.set_major_formatter(formatter)
+    axis1.xaxis.get_offset_text().set_size(10)
+    #axis1.yaxis.set_major_formatter(formatter)
+    axis1.yaxis.get_offset_text().set_size(10)
     axis1.set_title(f'Initially')
     axis1.set_ylabel('Number of events')
     axis1.tick_params(axis='both', labelsize=10)
@@ -68,6 +59,10 @@ def histogram(data_before, data_normalized, data_standardized, name):
     axis1.legend(loc='upper right', fontsize=10, fancybox=False, edgecolor='black')
 
     axis2 = axes[1]
+    #axis2.xaxis.set_major_formatter(formatter)
+    axis2.xaxis.get_offset_text().set_size(10)
+    #axis2.yaxis.set_major_formatter(formatter)
+    axis2.yaxis.get_offset_text().set_size(10)
     axis2.set_title(f'Normalized')
     axis2.tick_params(axis='both', labelsize=10)
     axis2.hist(data_normalized, density=True, histtype='step', bins=bins_count_normalized, label=name)
@@ -75,15 +70,6 @@ def histogram(data_before, data_normalized, data_standardized, name):
     axis2.plot([], [], ' ', label=f'Std Dev: {standard_deviation_normalized:.3f}')
     axis2.plot([], [], ' ', label=f'Bins: {len(bins_count_normalized)}')
     axis2.legend(loc='upper right', fontsize=10, fancybox=False, edgecolor='black')
-
-    axis3 = axes[2]
-    axis3.set_title(f'Standardized')
-    axis3.tick_params(axis='both', labelsize=10)
-    axis3.hist(data_standardized, density=True, histtype='step', bins=bins_count_standardized, label=name)
-    axis3.plot([], [], ' ', label=f'Mean: {mean_value_standardized:.3f}')
-    axis3.plot([], [], ' ', label=f'Std Dev: {standard_deviation_standardized:.3f}')
-    axis3.plot([], [], ' ', label=f'Bins: {len(bins_count_standardized)}')
-    axis3.legend(loc='upper right', fontsize=10, fancybox=False, edgecolor='black')
 
     plt.show()
     plt.close()
