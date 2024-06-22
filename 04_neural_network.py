a89e95f656a5f45e42a73fafbd590f11e122aafa
Rustam <azimzadeh.rustam@gmail.com>
2024-06-23 03:45:18 +0400

    added neural network output histograms

--- 04_neural_network.py
+++ 04_neural_network.py
@@ -1,13 +1,11 @@
 import keras.optimizers
-from keras.models import Sequential
-from keras.layers import Dense
+import numpy as np
 from matplotlib.ticker import ScalarFormatter
 from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 import matplotlib
 import matplotlib.pyplot as plt
 import scienceplots
@@ -32,33 +30,33 @@ def main():
     input_data = total_data.drop(columns=['signal'])
     output = pd.Series(total_data['signal'])
 
-    input_train, input_test, output_train, output_test = train_test_split(input_data, output, test_size=0.3,
+    input_train, input_test, output_train, output_test = train_test_split(input_data, output,
+                                                                          test_size=0.3,
                                                                           shuffle=True)
 
     neural_network = Sequential([
         Dense(units=24, input_dim=24, activation='relu'),
+        BatchNormalization(),
+        Dropout(0.5),
         Dense(units=128, activation='relu'),
+        BatchNormalization(),
         Dropout(0.5),
         Dense(units=64, activation='relu'),
+        BatchNormalization(),
+        Dropout(0.5),
         Dense(units=1, activation='sigmoid')
     ])
 
-    my_optimizer = keras.optimizers.SGD(learning_rate=0.001,
-                                        momentum=0.0,
-                                        nesterov=True)
+    my_optimizer = keras.optimizers.Adam(learning_rate=0.001)
 
-    neural_network.compile(optimizer=my_optimizer,
-                           loss='binary_crossentropy')
+    neural_network.compile(optimizer=my_optimizer, loss='binary_crossentropy')
 
-    neural_network.fit(input_train,
-                       output_train,
-                       epochs=50,
-                       batch_size=256,
-                       verbose=1)
+    neural_network.fit(input_train, output_train, epochs=150, batch_size=256, verbose=1)
 
     neural_network.save('tHbq_signal_classification.h5')
 
     plot_roc_curve(neural_network, input_test, output_test)
+    plot_histogram_of_predictions(neural_network, input_test, output_test)
 
 
 def normalize(data, max_value=None, min_value=None):
@@ -73,10 +71,9 @@ def normalize(data, max_value=None, min_value=None):
     return data, max_value, min_value
 
 
-def plot_roc_curve(model, input, output):
-    # Предсказание вероятностей для класса 1 (positive class)
-    predictions = model.predict(input).ravel()
-    fpr, tpr, thresholds = roc_curve(output, predictions)
+def plot_roc_curve(model, inputs_data, outputs):
+    predictions = model.predict(inputs_data).ravel()
+    fpr, tpr, thresholds = roc_curve(outputs, predictions, drop_intermediate=False)
     roc_auc = auc(fpr, tpr)
 
     plt.style.use(['science', 'notebook', 'grid'])
@@ -89,11 +86,34 @@ def plot_roc_curve(model, input, output):
     plt.ylabel('True Positive Rate', fontsize=14)
     plt.xlabel('False Positive Rate', fontsize=14)
     plt.tick_params(axis='both', labelsize=14)
-    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.plot(fpr, tpr, color='blue', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='red', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
     plt.legend(loc='best', fontsize=14, fancybox=False, edgecolor='black')
+    plt.show()
+
+
+def plot_histogram_of_predictions(model, inputs_data, outputs):
+    predictions = model.predict(inputs_data).ravel()
+    signal_predictions = predictions[outputs == 1]
+    signal_predictions_bins = np.histogram_bin_edges(signal_predictions, bins='scott')
+    background_predictions = predictions[outputs == 0]
+    background_predictions_bins = np.histogram_bin_edges(signal_predictions, bins='scott')
+
+    plt.style.use(['science', 'notebook', 'grid'])
+    matplotlib.rcParams.update({'font.size': 14})
+    formatter = ScalarFormatter(useMathText=True)
+    formatter.set_scientific(True)
+
+    plt.figure()
+    plt.title('Histogram of Neural Network Output', fontsize=14)
+    plt.xlabel('Predicted Probability', fontsize=14)
+    plt.ylabel('Frequency', fontsize=14)
+    plt.tick_params(axis='both', labelsize=14)
+    plt.hist(signal_predictions, bins=signal_predictions_bins, alpha=0.5, label='Signal (tHbq)', color='green')
+    plt.hist(background_predictions, bins=background_predictions_bins, alpha=0.8, hatch='//', histtype='step', label='Background', color='red')
+    plt.legend(loc='upper center', fontsize=14, fancybox=False, edgecolor='black')
     plt.show()
 
 
